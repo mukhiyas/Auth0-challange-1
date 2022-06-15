a24cb68a42b0dd1050794cbecb105da218da0a50
mukhiyas <34037476+mukhiyas@users.noreply.github.com>
2022-06-15 23:42:43 +0100

    Update vue.config.js

--- Pizza-challange/vue.config.js
+++ Pizza-challange/vue.config.js
@@ -2,8 +2,8 @@ const webpack = require('webpack')
 
 module.exports = {
   devServer: {
-    port: 3000,
-    proxy: 'http://localhost:3001'
+    port:$PORT,
+    proxy: ''
   },
   configureWebpack: {
     plugins: [
